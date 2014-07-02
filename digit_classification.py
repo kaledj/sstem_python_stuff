2c3b04f00e196f1841ef1983ebd5a0fd69fb6984
Scott <scottshuffler@gmail.com>
2014-07-02 12:33:36 -0400

    Stuff

--- digit_classification.py
+++ digit_classification.py
@@ -1,43 +1,45 @@
-import pylab as pl 
+import pylab as pl
 import numpy as np
 from sklearn import datasets
 
 
-##
-##	SCOTT WRITE HELPER FUNCTIONS PLS TYHNANZ
+# #
+##	SCOTT WRITE HELPER FUNCTIONS PLS THANKS
 ##
 def distanceAndClassify(y):
-	dis = np.zeros(10)
-	for z in range(0,10):
-		for q in range(0,64):
-			d1 = int(np.absolute(m[z][q] - y[q]))^2
-			dis[z] += d1
-		dis[z] = np.sqrt(dis[z])
-	lowest = np.argmin(dis)
-	if lowest == 0:
-		numlist0.append(y)
-	elif lowest == 1:
-		numlist1.append(y)
-	elif lowest == 2:
-		numlist2.append(y)
-	elif lowest == 3:
-		numlist3.append(y)
-	elif lowest == 4:
-		numlist4.append(y)
-	elif lowest == 5:
-		numlist5.append(y)
-	elif lowest == 6:
-		numlist6.append(y)
-	elif lowest == 7:
-		numlist7.append(y)
-	elif lowest == 8:
-		numlist8.append(y)
-	else:
-		numlist9.append(y)
+    dis = np.zeros(10)
+    for z in range(0, 10):
+        for q in range(0, 64):
+            d1 = int(np.absolute(m[z][q] - y[q])) ^ 2
+            dis[z] += d1
+        dis[z] = np.sqrt(dis[z])
+    lowest = np.argmin(dis)
+    if lowest == 0:
+        numlist0.append(y)
+    elif lowest == 1:
+        numlist1.append(y)
+    elif lowest == 2:
+        numlist2.append(y)
+    elif lowest == 3:
+        numlist3.append(y)
+    elif lowest == 4:
+        numlist4.append(y)
+    elif lowest == 5:
+        numlist5.append(y)
+    elif lowest == 6:
+        numlist6.append(y)
+    elif lowest == 7:
+        numlist7.append(y)
+    elif lowest == 8:
+        numlist8.append(y)
+    else:
+        numlist9.append(y)
+
 ## THANKS SCOTT
+## WELCOME KALE
 
 
-# Load the digits dataset
+# Load the digits data set
 digits = datasets.load_digits()
 print(digits.images)
 
@@ -52,7 +54,6 @@ numlist8 = []
 numlist9 = []
 numlist0 = []
 
-
 '''
  K-Means clustering
 '''
@@ -61,12 +62,12 @@ numlist0 = []
 # Randomly initialize solution as vectors of means m(t=0)=[m1...mk] 
 m = np.empty((10, 64))
 for i in range(0, 10):
-	m[i] = np.random.random_integers(0, 16, 64)
+    m[i] = np.random.random_integers(0, 16, 64)
 #print(m[0:])
 
 # Classify input data according to m(t=0)
 for image in digits.images:
-	distanceAndClassify(image.flatten())
+    distanceAndClassify(image.flatten())
 
 print len(numlist0)
 print len(numlist1)
@@ -80,11 +81,11 @@ print len(numlist8)
 print len(numlist9)
 
 
-	
- 
 
-	#print image
-	#print distance(1, 5)
+
+
+#print image
+#print distance(1, 5)
 
 # Use classification to compute m(t+1) 
 
@@ -92,6 +93,4 @@ print len(numlist9)
 
 # Check for convergence ||m(t) - m(t-1)|| < convergence threshold
 
-if __name__ == '__main__':
-	main()
 
