c33b2d0b4d2c40274097b83b5287a62185a72ddb
Scott <scottshuffler@gmail.com>
2014-07-11 09:21:38 -0400

    Kinda works

--- digit_classification.py
+++ digit_classification.py
@@ -2,13 +2,14 @@ import pylab as pl
 import numpy as np
 from sklearn import datasets
 from sklearn import metrics
+from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
 from numpy import random
 
 
 # #
 # #	SCOTT WRITE HELPER FUNCTIONS PLS THANKS
-##
+# #
 def distanceAndClassify(y):
     dis = np.zeros(10)
     for z in range(10):
@@ -18,15 +19,26 @@ def distanceAndClassify(y):
         dis[z] = np.sqrt(dis[z])
     return np.argmin(dis)
 
-def score(q,r):
-    estimate = KMeans(init='k-means++', n_clusters=10, n_init=10)
-    estimator = estimate.fit(q)
-    print ('Adj. Mutual Info Score: %.3f' % (metrics.adjusted_mutual_info_score(r, estimator.labels_)))
-    print ('Norm Mutual Info Score: %.3f' % (metrics.normalized_mutual_info_score(r, estimator.labels_)))
-    print ('Adj. Rand Score: %.3f' % (metrics.adjusted_rand_score(r, estimator.labels_)))
-    print ('Silhouette Score: %.3f' % (metrics.silhouette_score(q, estimator.labels_, metric = 'euclidean')))
-    
 
+def score(clusterlist):
+    images = np.zeros((len(digits.images), 64))
+    labels = np.zeros((len(digits.images)))
+    count = 0
+    for x in clusterlist:
+        for x0, x1 in zip(x[0], x[1]):
+            images[count] = x0
+            labels[count] = x1
+            count += 1
+    estimate = KMeans(init='random', n_clusters=10)
+    estimator = estimate.fit(digits.data)
+    print ('Adj. Mutual Info Score: %.3f' % (metrics.adjusted_mutual_info_score(digits.target, labels)))
+    print ('Norm Mutual Info Score: %.3f' % (metrics.normalized_mutual_info_score(digits.target, labels)))
+    print ('Adj. Rand Score: %.3f' % (metrics.adjusted_rand_score(digits.target, labels)))
+    print ('Silhouette Score: %.3f' % (metrics.silhouette_score(images, labels, metric='euclidean')))
+    pca = PCA(n_components=len(images))
+    pca.fit(images)
+
+    print (pca.explained_variance_ratio_)
 ## THANKS SCOTT
 ## WELCOME KALE
 
@@ -51,32 +63,29 @@ for i in range(10):
     pl.subplot(2, 5, i + 1)
     pl.imshow(m[i].reshape((8, 8)), cmap=pl.cm.gray_r, interpolation='nearest')
     #pl.imshow(digits.images[i], cmap=pl.cm.gray_r, interpolation='nearest')
-pl.show()
-
+#pl.show()
 
 converged = False
 attemptnum = 0
+numlist = []
 while not converged:
     # Initialize classification lists for current step
     numlist = []
     for i in range(0, 10):
-        numlist.append([])
-    print "Attempt : %d" % attemptnum
-    print numlist
+        numlist.append(([], []))
+    print "Attempt: %d" % attemptnum
 
     # Classify input data according to m(t=0)
-    for image in digits.images:
+    for index, image in enumerate(digits.images):
         closest = distanceAndClassify(image.flatten())
-        numlist[closest].append(image.flatten())
+        numlist[closest][0].append(image.flatten())
+        numlist[closest][1].append(digits.target[index])
 
     # Recomputed the vector of means
     mlast = m.copy()
     for i in range(10):
-        if len(numlist[i]) > 0:
-            print len(numlist[i])
-            m[i].put(range(64), np.average(numlist[i], axis=0).astype(np.dtype(np.int32)))
-            print type(m[i])
-    print "Attempt: %d" % attemptnum
+        if len(numlist[i][0]) > 0:
+            m[i].put(range(64), np.average(numlist[i][0], axis=0).astype(np.dtype(np.int32)))
     attemptnum += 1
     if np.any(m - mlast) == 0:
         converged = True
@@ -87,5 +96,5 @@ for i in range(10):
     #pl.imshow(digits.images[i], cmap=pl.cm.gray_r, interpolation='nearest')
 pl.show()
 
-score(digits.data, digits.target)
+score(numlist)
 
